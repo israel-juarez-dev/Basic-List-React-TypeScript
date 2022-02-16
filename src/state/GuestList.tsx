94428b3fb61dbc064e70799e6ea7cc24b04ebfcb
israel-juarez-dev <64047168+israel-juarez-dev@users.noreply.github.com>
2022-02-16 11:31:34 -0600

    test1

---
+++ src/state/GuestList.tsx
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+
+
+
+const GuestList : React.FC = () => {
+    const [name, setName] = useState<string>('');
+    const [guests, setGuests] = useState<string[]>([]);
+    const onClick = () => {
+        setName('');
+        setGuests([...guests, name])
+        
+    } 
+    return <div><h3>Guest List</h3>
+    {guests.map(guest => <li>{guest}</li>)}
+    <input type="text" value={name} onChange={(e) => setName(e.target.value) } />
+    <button onClick={onClick}>Add Guest</button>
+    </div>
+} 
+
+export default GuestList;
